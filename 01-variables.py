a3bef422f005610b43ae9bd57bdcf7a326e45fab
Crispurdygit config --global user.email cristian2basico@gmail.comgit config --global core.editor code <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-04-05 18:13:47 -0400

    mi primer repositorio

---
+++ 01-variables.py
@@ -0,0 +1,28 @@
+"""Este es un ejemplo"""
+
+#Esta es una impresion en pantalla
+print("Hola, soy Cristian!")
+
+#Se crean varias variables
+nombre = "Cristian"
+
+nombre2 = "Andres"
+
+apellido = "Cárdenas"
+
+apellido2 = "Lagos"
+
+edad = 18
+
+#Se imprime el valor de la variable con un mensaje
+print("Hola, soy ", nombre, nombre2, apellido, apellido2, "y tengo", edad, "años")
+print("Hola, soy" + " " + nombre + " " + nombre2 + " " + apellido + " " + apellido2 + " " + "y tengo" + " " + str(edad) + " " + "años")
+
+#Actualizacion de variables
+nombre = "Matias"
+
+apellido = "Guzman"
+print("Hola mi nuevo nombre es", nombre, apellido)
+
+nobre = input("¿Cual es tu nombre?:\n ")
+print("tu nombre es:", nombre)
