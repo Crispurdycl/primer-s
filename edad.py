af0c21e1cb721c1513710b9643bf412ffa992405
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-04-12 16:56:29 -0400

    Nuevo Archivo

---
+++ edad.py
@@ -0,0 +1,15 @@
+print("Ingresa tu edad\n")
+
+a = input()
+
+print("\n")
+
+print("Ingresa tu nombre:\n")
+
+b = input()
+
+print("\n")
+
+c = int(a) + 20
+
+print(f"mi nombre es {b}  y tengo {a} años y dentro de 20 años tendre {c}\n")
