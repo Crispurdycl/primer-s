d44c6d4c5c0fbdcb3e1b0225f873764eb727b6bf
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-05-02 15:10:30 -0400

    editado

--- 02-Reto-clase.py
+++ 02-Reto-clase.py
@@ -1,11 +1,20 @@
+#Definimos variables
+
 estudiante = input("Ingrese nombre del estudiante: ")
 carrera = input("Ingrese carrera del estudiante: ")
 NotaLab1 = float(input("Ingrese nota del laboratorio 1: "))
 NotaLab2 = float(input("Ingrese nota del laboratorio 2: "))
 
+#asignamos nuestro diccionario
+
 ICINF = [ estudiante, carrera, NotaLab1, NotaLab2 ]
+
+#calculamos el promedio de notas
 
 NotasLabs = NotaLab1*0.3 + NotaLab2*0.7
 
+#imprimimos el diccionario y el promedio de notas
+
+print(type(ICINF))
 print(ICINF, "\n")
 print(f" su promedio de notas es {NotasLabs:.1f}".format(NotasLabs))
