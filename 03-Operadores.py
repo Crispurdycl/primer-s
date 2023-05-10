413c1505fdc1b257b8184eb4012a13c74e1ef150
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-05-10 18:14:05 -0400

    Nuevos

---
+++ 03-Operadores.py
@@ -0,0 +1,29 @@
+a = 20
+b = 4
+c = 69
+d = 3
+print("Suma: ", a + b)
+print("Resta: ", a - b)
+print("exponente: ", a ** b)
+#FALTAN MAS OPERADORES
+
+t = 5.19
+g = 9.81
+v = g*t
+print(f"LA VELOCIDAD DEL OBJETO EN CAIDA LIBER ES: {v} m/s")
+print(f"LA VELOCIDAD DEL OBJETO EN CAIDA LIBER ES: {v:.2f} m/s")
+print("la velocidad del objeto en caida libre es: {:.2f}" .format(v), "m/s")
+
+#complejos
+c2=complex(3,-5)
+print("El numero complejo es: ", c2)
+
+print(c2.real)
+print(c2.imag)
+
+#a pobrar
+print("Hola"*5)
+print("Hola"*2)
+
+if not bencina or (encendido and edad >= 18):
+    print("Puede manejar")
--- calculadora.py
+++ calculadora.py
@@ -9,9 +9,9 @@ def Multiplicacion (a, b):
 def Division (a, b):
     return a / b
 def Potencia (a, b):
-    return a ** b
-def Raiz (a, b):
-    return sqrt(a)
+    return aEb
+def Raiz (a):
+    return a ** (1/2)
 
 print("Seleccione una operacion\n")
 print("1. Suma")
@@ -24,19 +24,27 @@ print("6. Raiz")
 while True:
     eleccion = input(" ( 1 | 2 | 3 | 4 | 5 | 6 )\n ")
     if eleccion in ("1", "2", "3", "4", "5", "6"):
-        num1=float(input("Por favor ingresa tu PRIMER numero: "))
-        num2=float(input("Por favor ingresa tu SEGUNDO numero: "))
-
         if eleccion == "1":
+            num1=float(input("Por favor ingresa tu PRIMER numero: "))
+            num2=float(input("Por favor ingresa tu SEGUNDO numero: "))
             print(f"{num1} + {num2} =", Suma (num1, num2))
         elif eleccion == "2":
+            num1=float(input("Por favor ingresa tu PRIMER numero: "))
+            num2=float(input("Por favor ingresa tu SEGUNDO numero: "))
             print(f"{num1} - {num2} =", Resta (num1, num2))
         elif eleccion == "3":
+            num1=float(input("Por favor ingresa tu PRIMER numero: "))
+            num2=float(input("Por favor ingresa tu SEGUNDO numero: "))
             print(f"{num1} * {num2} =", Multiplicacion (num1, num2))
         elif eleccion == "4":
+            num1=float(input("Por favor ingresa tu PRIMER numero: "))
+            num2=float(input("Por favor ingresa tu SEGUNDO numero: "))
             print(f"{num1} / {num2} =", Division (num1, num2))
         elif eleccion == "5":
+            num1=float(input("Por favor ingresa tu BASE: "))
+            num2=float(input("Por favor ingresa tu EXPONENTE: "))
             print(f"{num1} ** {num2} =", Potencia (num1, num2))
         elif eleccion == "6":
-            print(f"La raiz cuadrada es {num1}", Raiz (num1)) 
+            num1=float(input("Por favor ingresa tu numero: "))
+            print(f"La raiz cuadrada de {num1} es", Raiz (num1))
             break
