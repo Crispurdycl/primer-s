ea4f9dface0011ed1216d1896bd7965be5149d61
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-05-25 14:29:58 -0400

    actualizado

---
+++ Guias.2/algoritmo.py
@@ -0,0 +1,12 @@
+numero = int(input("Ingrese un numero: "))        
+impar = (numero*(numero-1))+1
+acum = 0
+
+for i in range(numero, numero*3, 2):
+    acum += impar
+    if i == (numero-1):
+        break 
+    impar += 2 
+    print(impar-2)
+
+print(f"El cubo de {numero} es: {acum}")  
