952d1f715eafc828189331bcacce557d376fff2c
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2023-05-26 22:43:00 -0400

    ss

--- Guias/Exercise-01.py
+++ Guias/Exercise-01.py
@@ -1,7 +1,7 @@
 # 1. Realizar un algoritmo que indique el número menor y el número mayor entre tres
 # enteros leidos por consola. Solo se deben ingresar números enteros.
 
-num1 = int(input("eri me la pela: "))
+num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 num3 = int(input("Ingrese el tercer numero: "))
 
